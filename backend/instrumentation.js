fd06555983c29790d0345d817317ea6ccd800bb3
bakir004 <bakir.cinjarevic@protonmail.com>
2025-12-11 12:47:13 +0100

    ffs

--- backend/instrumentation.js
+++ backend/instrumentation.js
@@ -3,7 +3,9 @@ const { NodeSDK } = require("@opentelemetry/sdk-node");
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
-const { OTLPTraceExporter } = require("@opentelemetry/exporter-otlp-http");
+const {
+  OTLPTraceExporter,
+} = require("@opentelemetry/exporter-trace-otlp-http");
 const { Resource } = require("@opentelemetry/resources");
 const {
   SemanticResourceAttributes,
